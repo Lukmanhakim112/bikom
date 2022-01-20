2cacfd5b1df8a40d5c38a680bebbb5455e38b288
Lukman Hakim <hlukman156@gmail.com>
2022-01-20 11:03:38 +0700

    organize admin page for CustomUser model

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'full_name', 'phone_number', 'is_staff', 'is_active')
+    list_filter = ('username', 'full_name', 'phone_number', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'full_name', 'phone_number', 'photo', 'password')}),
+        ('Permissions', {'fields': ('is_superuser', 'is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'is_superuser', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('username', 'phone_number')
+    ordering = ('username',)
 
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
+
